5fe0c82e40b460f63e57a5d5d3028dd280773bc5
jstnknr <143372221+jstnknr@users.noreply.github.com>
2024-02-12 22:43:13 -0500

    Random word chosen, variables defined.

--- index.js
+++ index.js
@@ -12,8 +12,35 @@ var words = [
   'mango'
 ]
 
+var previousWord = document.getElementById("previous-word")
+previousWord.innerHTML = "This is not listed anywhere in our lectures. I promise I am trying and spending way to much time watching the lecture and rereading material."
+var incorrectLetters = document.getElementById("incorrect-letters")
+incorrectLetters = []
+incorrectLetters.innerHTML = incorrectLetters
+var remainingGuesses = document.getElementById("remaining-guesses")
+remainingGuesses.innerHTML = 10
+var wins = document.getElementById("wins")
+var losses = document.getElementById("losses")
+wins = 0
+losses = 0
+
+var wordToGuess = document.getElementById("word-to-guess")
+
+var word = words[Math.floor(Math.random() * words.length)]
+console.log(word)
+
+
+
+wordToGuess.innerHTML = word
+//var underscore = word.length 
+//wordToGuess.innerHTML = underscore.replace(
+  
+
+
+ 
 //select a word at random and place it in word to guess element
 //pick word random, replace #word-to-guess, replace letters with underscores. Display ten remaping guesses number in #remaing guesses
+
 
 
 //keyup, check if letter is in word. 
